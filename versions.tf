25ca631599a9ecb7627c7e547bf7bfc7fedc76c9
Ingo Becker <ingo@orgizm.net>
2022-08-09 10:56:28 +0200

    Update tf and provider versions

--- versions.tf
+++ versions.tf
@@ -1,6 +1,17 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = "~> 1.2.6"
   required_providers {
+
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.6.14"
+    }
+
+    ct = {
+      source  = "poseidon/ct"
+      version = "0.11.0"
+    }
+
     local    = "~> 1.2"
     template = "~> 2.1"
   }
